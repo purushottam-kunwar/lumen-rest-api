0340f13c126f26e81cfedab74da4e961deef0b5d
Purushottam Kunwar <107859674+pk-savitriya@users.noreply.github.com>
2022-09-06 21:40:22 +0545

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('/posts', 'PostController@index');
+    $router->post('/post ', 'PostController@store');
+    $router->put('/post/{id}', 'PostController@update');
+    $router->delete('/post/{id}', 'PostController@destroy');
+});
